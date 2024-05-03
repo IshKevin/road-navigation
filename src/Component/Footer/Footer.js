28ee9d0c28882c4bb4c9d189d42b4a4bbca2b838
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-05-04 00:28:19 +0200

    chore: Add Footer component and its styles

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <>
+    <footer className="footer">
+        <div className="footer-content">
+          <div className="location-icon">Location Icon</div>
+          <div className="location-name">Kigali</div>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
