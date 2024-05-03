6edc89d4f069e90f75a476d67557bfbfa9e7acab
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-05-04 00:28:35 +0200

    chore: Add Header component and its styles

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+     <header className="header">
+        <div className="header-content">
+          <div className="app-name">Startup</div>
+          <div className="route-info">
+            <div className="route-title">Nyabugogo - Kimironko</div>
+            <div className="route-details">Next stop: Kicyiru Eta park | Distance: 2.8 km | Time: 23 minutes</div>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
