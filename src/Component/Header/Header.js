7c2460fc5cd146643894bec46722b443e0d1b12e
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-05-04 15:24:55 +0200

    Update Header component styles and add app icon

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './Header.css'
+import { IoMdMenu } from "react-icons/io";
 
 function Header() {
   return (
     <>
      <header className="header">
         <div className="header-content">
+          <div className='app-icon'><IoMdMenu /></div>
           <div className="app-name">Startup</div>
-          <div className="route-info">
-            <div className="route-title">Nyabugogo - Kimironko</div>
-            <div className="route-details">Next stop: Kicyiru Eta park | Distance: 2.8 km | Time: 23 minutes</div>
-          </div>
         </div>
       </header>
     </>
@@ -18,3 +16,8 @@ function Header() {
 }
 
 export default Header
+
+/* <div className="route-info">
+            <div className="route-title">Nyabugogo - Kimironko</div>
+            <div className="route-details">Next stop: Kicyiru Eta park | Distance: 2.8 km | Time: 23 minutes</div>
+          </div>*/
