d7f2a79a1aca0d8949949e23013d536e9cd68dd5
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-05-04 15:24:38 +0200

    Update Footer component styles and icons

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './Footer.css'
+import { CiHeart } from "react-icons/ci";
+import { CiCircleInfo } from "react-icons/ci";
+import { MdNotificationsNone } from "react-icons/md";
 
 function Footer() {
   return (
     <>
     <footer className="footer">
-        <div className="footer-content">
-          <div className="location-icon">Location Icon</div>
-          <div className="location-name">Kigali</div>
-        </div>
+        <div className="location-icon"><CiHeart /></div>
+        <div className="location-name"><CiCircleInfo /></div>
+        <div className="location-name"><MdNotificationsNone /></div>
       </footer>
     </>
   )
